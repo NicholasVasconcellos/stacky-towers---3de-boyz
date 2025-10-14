133a58e0d034ab8386aa7326a515cd38f22f932b
Nicholas <nicholas.svasc@gmail.com>
2025-10-14 17:20:30 +0900

    Make Camera Follow Player

--- CameraController.cs
+++ CameraController.cs
@@ -1,15 +1,15 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class CameraController : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() { }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        CharacterBody3D character = GetTree().GetNodesInGroup("Player")[0] as CharacterBody3D;
+        GlobalPosition = character.GlobalPosition;
+    }
 }
