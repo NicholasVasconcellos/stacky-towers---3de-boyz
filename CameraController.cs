d1c79e276ada9448ac693b57a8fb245a7aba3576
Nicholas <nicholas.svasc@gmail.com>
2025-10-14 18:30:41 +0900

    Add Camera Movement with mouse

--- CameraController.cs
+++ CameraController.cs
@@ -3,13 +3,45 @@ using Godot;
 
 public partial class CameraController : Node3D
 {
+    private float hRotation = 0f;
+    private float vRotation = 0f;
+
+    [Export]
+    public float sensitivity = 0.3f;
+
+    [Export]
+    public float maxPitch = 60f;
+
+    [Export]
+    public float minPitch = -30f;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() { }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
+    public override void _Process(double delta) { }
+
+    // Called on Every Input
+    public override void _Input(InputEvent @event)
     {
-        CharacterBody3D character = GetTree().GetNodesInGroup("Player")[0] as CharacterBody3D;
-        GlobalPosition = character.GlobalPosition;
+        base._Input(@event);
+
+        if (@event is InputEventMouseMotion motion)
+        {
+            // Get the mosue offset
+            Vector2 relativeMotion = motion.Relative;
+
+            // update the Horizontal rotation
+            hRotation -= relativeMotion.X * sensitivity;
+
+            // update the Vertical rotation
+            vRotation -= relativeMotion.Y * sensitivity;
+
+            // Calmp the Vertical Rotation
+            vRotation = Mathf.Clamp(vRotation, minPitch, maxPitch);
+
+            // Update Object Rotation
+            RotationDegrees = new Vector3(0, hRotation, vRotation);
+        }
     }
 }
