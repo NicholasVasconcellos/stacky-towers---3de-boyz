c69e4f5132e88b6af202c7a20b3ead484c631228
Drayiss <nolankwa@gmail.com>
2025-10-19 16:08:24 +0900

    Revert "Makes progress toward implementing Gobot as player model (still not working movement yet though"
    This reverts commit 8a46b603efc30d6cfed6f8dc337b83045113c048.

--- CameraController.cs
+++ CameraController.cs
@@ -1,64 +1,64 @@
-// using System;
-// using System.Runtime.CompilerServices;
-// using Godot;
-//
-// public partial class CameraController : Node3D
-// {
-//     private float hRotation = 0f;
-//     private float vRotation = 0f;
-//
-//     [Export]
-//     public float sensitivity = 0.3f;
-//
-//     [Export]
-//     public float maxPitch = 60f;
-//
-//     [Export]
-//     public float minPitch = -30f;
-//
-//     [Export]
-//     public double hAcceleration = 2.0;
-//
-//     [Export]
-//     public double vAcceleration = 2.0;
-//
-//     // Called when the node enters the scene tree for the first time.
-//     public override void _Ready() { }
-//
-//     // Called every frame. 'delta' is the elapsed time since the previous frame.
-//     public override void _Process(double delta) { }
-//
-//     // Called on Every Input
-//     public override void _Input(InputEvent @event)
-//     {
-//         base._Input(@event);
-//
-//         if (@event is InputEventMouseMotion motion)
-//         {
-//             // Get the mosue offset
-//             Vector2 relativeMotion = motion.Relative;
-//
-//             // update the Horizontal rotation
-//             hRotation -= relativeMotion.X * sensitivity;
-//
-//             // update the Vertical rotation
-//             vRotation -= relativeMotion.Y * sensitivity;
-//
-//             // Calmp the Vertical Rotation
-//             vRotation = Mathf.Clamp(vRotation, minPitch, maxPitch);
-//
-//             // Update Object Rotation
-//             RotationDegrees = new Vector3(0, hRotation, vRotation);
-//         }
-//     }
-//
-//     //for doing joystick rotation
-//     private void RotateFromVector(Vector2 V)
-//     {
-//         if (V.Length() == 0)
-//         {
-//             return;
-//         }
-//         Rotation = new Vector3(Rotation.X, Rotation.Y + V.X, Rotation.Z);
-//     }
-// }
+using System;
+using System.Runtime.CompilerServices;
+using Godot;
+
+public partial class CameraController : Node3D
+{
+    private float hRotation = 0f;
+    private float vRotation = 0f;
+
+    [Export]
+    public float sensitivity = 0.3f;
+
+    [Export]
+    public float maxPitch = 60f;
+
+    [Export]
+    public float minPitch = -30f;
+
+    [Export]
+    public double hAcceleration = 2.0;
+
+    [Export]
+    public double vAcceleration = 2.0;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() { }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) { }
+
+    // Called on Every Input
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+
+        if (@event is InputEventMouseMotion motion)
+        {
+            // Get the mosue offset
+            Vector2 relativeMotion = motion.Relative;
+
+            // update the Horizontal rotation
+            hRotation -= relativeMotion.X * sensitivity;
+
+            // update the Vertical rotation
+            vRotation -= relativeMotion.Y * sensitivity;
+
+            // Calmp the Vertical Rotation
+            vRotation = Mathf.Clamp(vRotation, minPitch, maxPitch);
+
+            // Update Object Rotation
+            RotationDegrees = new Vector3(0, hRotation, vRotation);
+        }
+    }
+
+    //for doing joystick rotation
+    private void RotateFromVector(Vector2 V)
+    {
+        if (V.Length() == 0)
+        {
+            return;
+        }
+        Rotation = new Vector3(Rotation.X, Rotation.Y + V.X, Rotation.Z);
+    }
+}
