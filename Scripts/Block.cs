5d085a6e9b2d0e3d22089a78087a8c962c1e95ef
Nicholas <nicholas.svasc@gmail.com>
2025-11-12 14:22:00 +0900

    remove example Block script (old)

--- Scripts/Block.cs
+++ Scripts/Block.cs
@@ -5,4 +5,9 @@ public partial class Block : RigidBody3D
 {
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() { }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+    }
 }
