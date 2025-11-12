90c5d5ea09adc2a3f928e848d1c2bfc4a869e56e
Drayiss <nolankwa@gmail.com>
2025-11-12 15:44:43 +0900

    Adds color to jetpack particles, jetpack audio, and API for jetpack

---
+++ Scripts/Jetpack.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+
+public partial class Jetpack : Node3D
+{
+	// Node references
+	private GpuParticles3D _particles;
+	private AudioStreamPlayer3D _sound;
+	
+	public override void _Ready()
+	{
+		// Get the nodes
+		_particles = GetNode<GpuParticles3D>("ThrusterParticles");
+		_sound = GetNode<AudioStreamPlayer3D>("ThrusterSound");
+	}
+
+	/// <summary>
+	/// Turns on all jetpack visual and audio effects.
+	/// </summary>
+	public void StartEffects()
+	{
+		if (!_particles.Emitting)
+		{
+			_particles.Emitting = true;
+		}
+
+		if (!_sound.IsPlaying())
+		{
+			_sound.Play();
+		}
+	}
+
+	/// <summary>
+	/// Turns of all jetpack visual and audio effects.
+	/// </summary>
+	public void StopEffects()
+	{
+		if (_particles.Emitting)
+		{
+			_particles.Emitting = false;
+		}
+
+		if (_sound.IsPlaying())
+		{
+			_sound.Stop();
+		}
+	}
+}
